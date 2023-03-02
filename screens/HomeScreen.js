125f4d11a39a7ce30d6c185971d62ceca5145545
parth <admin@test.com>
2023-03-02 23:41:49 +0530

    added componenets

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,23 @@
-import { View, Image, SafeAreaView } from 'react-native'
+import { View, Image, SafeAreaView, Text, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { ChevronDownIcon, UserIcon, MagnifyingGlassIcon,AdjustmentsVerticalIcon } from 'react-native-heroicons/outline'
+import Categories from '../components/categories';
+
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
       navigation.setOptions({
-        headerShown: true,
+        headerShown: false,
       })
 },[])
   
   
   return ( 
-    <SafeAreaView>
+    <SafeAreaView className='bg-white pt-5'>
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         {/* Header */}
          <View>
           <Image
@@ -23,6 +27,39 @@ const HomeScreen = () => {
             className = "h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
          </View>
+
+
+         <View className='flex-1'>
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className='font-bold text-xl'>Current Location
+            <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+         </View>
+
+         <UserIcon size={35} color="#00ccbb" />
+
+
+    </View>
+
+    {/* Search */}
+    <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+      <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+        <MagnifyingGlassIcon color="gray" size={20} />
+        <TextInput placeholder="Search Dishes or Restaurants" keyboardType='default'/>
+      </View>
+
+      <AdjustmentsVerticalIcon color='#00ccbb'/>
+    </View>
+
+    {/* Body */}
+    <ScrollView>
+      {/* Categories slider */}
+          <Categories />
+
+      {/* Featured */}
+      {/* featured rows */}
+    </ScrollView>
+
     </SafeAreaView>
   );
 };
